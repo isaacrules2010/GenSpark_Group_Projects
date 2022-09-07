5b5bd78a42d7b5937dac7341f056ede589867c01
Isaacrules2010 <williamcunningham32@gmail.com>
2022-09-07 14:46:19 -0500

    Value Class
    added then 10s place, still no logic to USE the info though

--- CodingChallenge1/src/java/Value.java
+++ CodingChallenge1/src/java/Value.java
@@ -3,12 +3,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Value {
-    int value;
-    String name;
+    /*
+    * For now, why dont we just assume values less than one hundred?
+    * If that works, THEN we can worry about handling values 100 - 999
+    */
+
+
+    private int value;
+    private String name;
 
     List<String> ones = new ArrayList<String>(List.of("Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"));
+    List<String> tens = new ArrayList<String>(List.of("", "", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"));
 
     public Value(String name){
         // value = tens.indexOf + ones.indexOf
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
     }
 }
