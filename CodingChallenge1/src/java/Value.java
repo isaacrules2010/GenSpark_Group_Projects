797cccdc16b85a2d95892fb78af60b0a4344c3e4
Isaacrules2010 <williamcunningham32@gmail.com>
2022-09-07 14:37:56 -0500

    Value Class
    The class that stores the data that has been read from the file

---
+++ CodingChallenge1/src/java/Value.java
@@ -0,0 +1,14 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Value {
+    int value;
+    String name;
+
+    List<String> ones = new ArrayList<String>(List.of("Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"));
+
+    public Value(String name){
+        // value = tens.indexOf + ones.indexOf
+    }
+}
