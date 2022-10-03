6074e9972106ea501453ad661e11e083fd1b3c02
Isaacrules2010 <williamcunningham32@gmail.com>
2022-10-03 15:42:18 -0500

    Quick Character class
    Just for the sake ofgetting things started. If we need anything else, we/I can add it later. :)

---
+++ Full-Stack Project/spring_app/src/main/java/com/genspark/spring_app/Entity/Character.java
@@ -0,0 +1,100 @@
+package com.genspark.spring_app.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Character {
+    @Id
+    @Column(name = "char_id")
+    private int id;
+    private String name;
+    @Column(name = "class")
+    private String characterClass;
+
+    // These values shouldn't go above 20;
+    private int strength; //str
+    private int dexterity; //dex
+    private int constitution; //con
+    private int wisdom; //wis
+    private int intelligence; //int
+    private int charisma; //cha
+
+
+    //default constructor
+    public Character() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharacterClass() {
+        return characterClass;
+    }
+
+    public void setCharacterClass(String characterClass) {
+        this.characterClass = characterClass;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getConstitution() {
+        return constitution;
+    }
+
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getCharisma() {
+        return charisma;
+    }
+
+    public void setCharisma(int charisma) {
+        this.charisma = charisma;
+    }
+}
