a3fb3563a7fe53da0128ea656e0407c95a6cf62f
Ian Bright <iantom12@yahoo.com>
2022-10-07 18:52:33 -0400

    Home Background

--- Full-Stack Project/react_app/src/RoutingPages/Home.js
+++ Full-Stack Project/react_app/src/RoutingPages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "../Components/Nav";
+import '../Home.css';
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <Nav/>
     </div>
+
   )
 }
