1fd515f2c1f725d7b322ad957cf59b520a61d283
Kaymon McCain <kaymonCodeM@outlook.com>
2022-10-08 11:34:07 -0500

    Admin login

--- Full-Stack Project/react_app/src/RoutingPages/Admin.js
+++ Full-Stack Project/react_app/src/RoutingPages/Admin.js
@@ -1,11 +1,18 @@
-import React from "react";
-import LoginForm from "../Components/LoginForm";
+import React, { useEffect, useState } from "react";
 import Creator from "../Components/Creator";
 
 export default function Admin(){
+    const [adminDisplay,setAdminDisplay] = useState("none");
+
+    useEffect(() => {
+        if(localStorage.getItem('role')!=='admin'){
+            window.location.href="http://localhost:3000/admin/login"
+        }
+        setAdminDisplay('block');
+    },[]);
 
     return(
-        <div>
+        <div className="admin" style={{display:adminDisplay}}>
             <Creator/>
         </div>
     )
---
+++ Full-Stack Project/react_app/src/RoutingPages/LoginAdmin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AdminLoginForm from '../Components/AdminLoginForm'
+
+export default function LoginAdmin() {
+  return (
+    <div>
+        <AdminLoginForm />
+    </div>
+  )
+}
