f759cbc14aa89b768c7202e918db272a85e8f729
Kaymon McCain <kaymonCodeM@outlook.com>
2022-10-08 09:19:13 -0500

    Included Nav in the forms

--- Full-Stack Project/react_app/src/RoutingPages/Login.js
+++ Full-Stack Project/react_app/src/RoutingPages/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
 import LoginForm from "../Components/LoginForm";
+import Nav from "../Components/Nav";
 
 
 export default function Login() {
   return (
     <div>
+      <Nav />
       <LoginForm />
     </div>
   )
--- Full-Stack Project/react_app/src/RoutingPages/NewUser.js
+++ Full-Stack Project/react_app/src/RoutingPages/NewUser.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Nav from '../Components/Nav'
 import NewUserForm from '../Components/NewUserForm'
 
 export default function NewUser() {
   return (
     <div>
+      <Nav />
       <NewUserForm />
     </div>
   )
